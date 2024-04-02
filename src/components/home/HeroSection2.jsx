87f4d09d9ec575b531a03944227e93a86a5a4e91
Doctor Zangi <doctorzangi@gmail.com>
2024-04-02 10:54:29 +0500

    scrolling is okay

--- src/components/home/HeroSection2.jsx
+++ src/components/home/HeroSection2.jsx
@@ -1,37 +1,13 @@
 "use client";
 
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import Card from "../constants/Card";
 
 const Herosection2 = () => {
-  const leftDivRef = useRef(null); // Create a ref for the left div
-  const rightDivRef = useRef(null); // Create a ref for the right div
-  const [leftDivSticky, setLeftDivSticky] = useState(false); // State to track if the left div is sticky
-
-  useEffect(() => {
-    const handleScroll = () => {
-      // Check if the left div is fully in the viewport
-      if (leftDivRef.current) {
-        const { top, bottom } = leftDivRef.current.getBoundingClientRect();
-        setLeftDivSticky(top <= 0 && bottom >= window.innerHeight);
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
+ 
   return (
-    <div className="flex w-full h-screen bg-white">
-      <div
-        className={`w-1/3 pl-6 overflow-hidden ${
-          leftDivSticky ? "sticky top-0" : ""
-        }`}
-        ref={leftDivRef}
-      >
+    <div className="flex w-full bg-white relative" style={{ boxSizing:'border-box', backgroundAttachment:'scroll !important', unicodeBidi:"isolate"}}>
+      <div className= "w-1/3 pl-6 block sticky top-14 h-[100vh] ">
         <h2
           className="mt-6"
           style={{ fontSize: "1vw", color: "#ff9a54", fontWeight: "600" }}
@@ -46,11 +22,7 @@ const Herosection2 = () => {
           solutions.
         </p>
       </div>
-      <div
-        className="w-2/3 overflow-auto custom-scrollbar"
-        style={{ scrollbarWidth: "none" }}
-        ref={rightDivRef}
-      >
+      <div className="w-2/3">
         <div className="flex">
           <div className="flex flex-col">
             <Card
