0492686afd81aee66c320b7975e95884f7dc0029
Doctor Zangi <doctorzangi@gmail.com>
2024-04-06 13:55:41 +0500

    Modified some settings in EventCard

--- src/components/constants/events/EventCard.jsx
+++ src/components/constants/events/EventCard.jsx
@@ -9,7 +9,7 @@ const EventCard = ({ src, dateTime, title, content }) => {
         src={src}
         width={500}
         height={500}
-        className="w-full"
+        className="w-screen h-[50vw] md:h-[25vw] xl:h-[17vw] 2xl:h-[12.5vw]"
       />
       <div className="w-full flex flex-row items-center justify-between mt-[3vh]">
         <button className="uppercase text-blue-950 text-[0.7vw] bg-blue-100 py-[0.7vw] px-[1.5vw] rounded-full">
