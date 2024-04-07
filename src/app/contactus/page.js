149a1043bb1b361bf054bd4d21afc26b1911ea85
Doctor Zangi <doctorzangi@gmail.com>
2024-04-07 17:34:35 +0500

    added a contact us page

---
+++ src/app/contactus/page.js
@@ -0,0 +1,10 @@
+import ContactUs from "@/components/contactUs/ContactUs";
+
+export default function ContactPage () {
+return (
+    <div className="flex min-h-screen w-full px-[4vw] flex-col items-center justify-between bg-white">
+        <ContactUs />
+    </div>
+)
+
+}
