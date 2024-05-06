d61b02db2659ce5d06b105964823529a948ec7a2
Muhammad Junaid Aslam <15-10648@formanite.fccollege.edu.pk>
2024-05-06 17:50:40 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   images: {
     remotePatterns: [
       {
@@ -13,4 +13,4 @@ const nextConfig = {
     ],
   },
 };
-export default nextConfig;
+
