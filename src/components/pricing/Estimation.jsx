6a4957290ef189ad35b64895dbbc9a138fc52798
Doctor Zangi <doctorzangi@gmail.com>
2024-05-08 16:10:26 +0500

    questions issue fixed

--- src/components/pricing/Estimation.jsx
+++ src/components/pricing/Estimation.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import questions from "./questions";
+import questions from "./Questions";
 
 const Estimation = () => {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
